4fef4b3c6d7104403a382fe62f9e6ef77ee55f4a
Darkhan Zhursin <darkhan.zhursin@mediamonks.com>
2022-09-03 00:48:52 +0600

    Final Task

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        switch (divider) {
+            case (0):
+                System.out.println("division by zero");
+                break;
+            default:
+                int res1 = dividend / divider;
+                int res2 = res1 * divider;
+                if (res2 == dividend) {
+                    System.out.println("can be divided completely");
+                } else if (res2 < dividend || res2 > dividend) {
+                    System.out.println("cannot be divided completely");
+                }
+                break;
+        }
+
     }
 }
